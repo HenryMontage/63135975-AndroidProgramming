bb12d89a54aa824a670a1e79bf57618f8660aae4
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-03-06 21:11:22 +0700

    Ex2_LinearLayout_finish

---
+++ Ex2_LinearLayout/app/src/main/java/ntu63CNTT_2/ex2_linearlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu63CNTT_2.ex2_linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //Nạp layout
+    }
+}
