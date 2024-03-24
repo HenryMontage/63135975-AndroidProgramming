690cd169d7ec438f25990911806724f78348c786
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-03-24 12:11:25 +0700

    Them cac Icon va thiet lap bo cuc cho Main BMI

--- ntu_63135975/Cau2_AppBMI_Calculator/app/src/main/java/java/ntu_63135975/cau2_appbmi_calculator/MainActivity.java
+++ ntu_63135975/Cau2_AppBMI_Calculator/app/src/main/java/java/ntu_63135975/cau2_appbmi_calculator/MainActivity.java
@@ -13,17 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },3000);
     }
 }
---
+++ ntu_63135975/Cau2_AppBMI_Calculator/app/src/main/java/java/ntu_63135975/cau2_appbmi_calculator/OpenBMI.java
@@ -0,0 +1,29 @@
+package java.ntu_63135975.cau2_appbmi_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class OpenBMI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_bmi);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(OpenBMI.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
