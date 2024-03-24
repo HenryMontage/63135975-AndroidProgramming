825c31d91de0c324ea63d0bb7dc7e11e7d34e9ed
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-03-24 20:30:23 +0700

    Chinh sua push lan 4 season 2 (Co the co Season 3 )

--- ntu_63135975/Cau2_AppBMICalculator/app/src/main/java/java/ntu_63135975/cau2_appbmicalculator/MainActivity.java
+++ ntu_63135975/Cau2_AppBMICalculator/app/src/main/java/java/ntu_63135975/cau2_appbmicalculator/MainActivity.java
@@ -3,9 +3,11 @@ package java.ntu_63135975.cau2_appbmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
