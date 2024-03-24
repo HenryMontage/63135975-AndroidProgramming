6e1d5487b4cbe7d89e6ff16d9eea0ca50527b9a5
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-03-24 20:19:26 +0700

    Them Android Name: OpenBMI vao

--- ntu_63135975/Cau2_AppBMICalculator/app/src/main/java/java/ntu_63135975/cau2_appbmicalculator/OpenBMI.java
+++ ntu_63135975/Cau2_AppBMICalculator/app/src/main/java/java/ntu_63135975/cau2_appbmicalculator/OpenBMI.java
@@ -2,7 +2,10 @@ package java.ntu_63135975.cau2_appbmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class OpenBMI extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class OpenBMI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_bmi);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(OpenBMI.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
 }
