c2097bacffe6106f0921e681370b54a96ba70da4
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-03-24 15:49:19 +0700

    Hoan thanh Fontend app BMI

--- ntu_63135975/Cau2_AppBMI_Calculator/app/src/main/java/java/ntu_63135975/cau2_appbmi_calculator/BMIActivity.java
+++ ntu_63135975/Cau2_AppBMI_Calculator/app/src/main/java/java/ntu_63135975/cau2_appbmi_calculator/BMIActivity.java
@@ -5,26 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class BMIActivity extends AppCompatActivity {
 
     android.widget.Button mrecalculatebmi;
-    TextView mcurrentheight;
-    TextView mcurrentage, mcurrentweight;
-    ImageView mincrementage, mincrementweight, mdecrementweight, mdecrementage;
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mrecalculatebmi = findViewById(R.id.calculatebmi);
+        setContentView(R.layout.activity_bmi);
+        mrecalculatebmi = findViewById(R.id.recalculatebmi);
+
         mrecalculatebmi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, BMIActivity.class);
+                Intent intent = new Intent(BMIActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
--- ntu_63135975/Cau2_AppBMI_Calculator/app/src/main/java/java/ntu_63135975/cau2_appbmi_calculator/OpenBMI.java
+++ ntu_63135975/Cau2_AppBMI_Calculator/app/src/main/java/java/ntu_63135975/cau2_appbmi_calculator/OpenBMI.java
@@ -12,18 +12,17 @@ public class OpenBMI extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_open_bmi);
+        setContentView(R.layout.activity_openbmi);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(OpenBMI.this, MainActivity.class);
+                Intent intent=new Intent(OpenBMI.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        },4000);
     }
 }
