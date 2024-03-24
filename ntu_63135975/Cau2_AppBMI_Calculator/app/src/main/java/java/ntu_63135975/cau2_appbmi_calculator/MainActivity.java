cdd0eb414654db9333276931dfe3f8c3f7373703
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-03-24 11:02:11 +0700

    Khoi tao Cau2_BMI_Calculator

---
+++ ntu_63135975/Cau2_AppBMI_Calculator/app/src/main/java/java/ntu_63135975/cau2_appbmi_calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package java.ntu_63135975.cau2_appbmi_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
