cf4900afba893a0d8319e8f83e5413d710e8014a
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-03-24 20:33:31 +0700

    Chinh sua backend App lan 5

--- ntu_63135975/Cau2_AppBMICalculator/app/src/main/java/java/ntu_63135975/cau2_appbmicalculator/MainActivity.java
+++ ntu_63135975/Cau2_AppBMICalculator/app/src/main/java/java/ntu_63135975/cau2_appbmicalculator/MainActivity.java
@@ -3,15 +3,25 @@ package java.ntu_63135975.cau2_appbmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText
+    EditText ediWeight, ediFeet, ediInch;
+    Button buttonBmiCalculate;
+    TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ediWeight = findViewById(R.id.ediWeight);
+        ediFeet = findViewById(R.id.ediFeet);
+        ediInch = findViewById(R.id.ediInch);
+        buttonBmiCalculate = findViewById(R.id.buttonBmiCalculate);
+        tvResult = findViewById(R.id.tvResult);
+
 
     }
 }
