055b3623ac41f1310be939fbe42ac1cac2a81962
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-04-16 10:03:37 +0700

    Ex8_ExamList_ss1

---
+++ Ex8_ExamList/app/src/main/java/java/ntu63135975/ex8_examlist/ClickListiner.java
@@ -0,0 +1,10 @@
+package java.ntu63135975.ex8_examlist;
+
+
+public class ClickListiner{
+
+    // here index is index
+// of item clicked
+    public click(int index);
+
+}
---
+++ Ex8_ExamList/app/src/main/java/java/ntu63135975/ex8_examlist/examData.java
@@ -0,0 +1,16 @@
+package java.ntu63135975.ex8_examlist;
+
+public class examData {
+    String name;
+    String date;
+    String message;
+
+    examData(String name,
+             String date,
+             String message)
+    {
+        this.name = name;
+        this.date = date;
+        this.message = message;
+    }
+}
