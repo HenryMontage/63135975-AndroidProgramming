a06e9615a1b61edb4590fbe8b943ed0163411ab6
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-04-22 17:45:16 +0700

    viewpager2-in-android-with-example_finish

---
+++ Viewpager2_in_android_with_example/app/src/main/java/java/ntu63135975/viewpager2_in_android_with_example/FirstFragment.java
@@ -0,0 +1,20 @@
+package java.ntu63135975.viewpager2_in_android_with_example;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup
+            container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_first_fragment, container, false);
+    }
+}
---
+++ Viewpager2_in_android_with_example/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Viewpager2_in_android_with_example"
+include(":app")
+ 
