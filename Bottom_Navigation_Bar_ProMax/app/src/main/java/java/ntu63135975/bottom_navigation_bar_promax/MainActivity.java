bb12feae6db5ed3005a01cb8957a958ece175cc8
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-05-18 10:28:54 +0700

    Push_BottomPlus_BaiThiTongHop

---
+++ Bottom_Navigation_Bar_ProMax/app/src/main/java/java/ntu63135975/bottom_navigation_bar_promax/MainActivity.java
@@ -0,0 +1,14 @@
+package java.ntu63135975.bottom_navigation_bar_promax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Ex_BaiThiTongHop/app/src/main/java/java/ntu63135975/ex_baithitonghop/MainActivity.java
@@ -0,0 +1,14 @@
+package java.ntu63135975.ex_baithitonghop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
