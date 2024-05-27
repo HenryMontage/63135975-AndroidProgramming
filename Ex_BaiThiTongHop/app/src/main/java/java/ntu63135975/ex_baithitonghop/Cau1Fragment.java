f51af5c615157cb9b42fccd5ca97f79921c66cd1
Kiều Thái Tuấn <thaichualam123@gmail.com>
2024-05-27 15:05:54 +0700

    Finish_GG_BaiThiUpdate

--- Ex_BaiThiTongHop/app/src/main/java/java/ntu63135975/ex_baithitonghop/Cau1Fragment.java
+++ Ex_BaiThiTongHop/app/src/main/java/java/ntu63135975/ex_baithitonghop/Cau1Fragment.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,34 +18,16 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Cau1Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button buttonCong;
+    EditText editTextSo1,  editTextSo2;
 
     public Cau1Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Cau1Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Cau1Fragment newInstance(String param1, String param2) {
         Cau1Fragment fragment = new Cau1Fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +35,43 @@ public class Cau1Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau1, container, false);
+        View viewCau1 = inflater.inflate(R.layout.fragment_cau1, container, false);
+        //Tìm điều khiển trong này
+        buttonCong = viewCau1.findViewById(R.id.btnTinhTong);
+        editTextSo1 = viewCau1.findViewById(R.id.editTextSoA);
+        editTextSo2 = viewCau1.findViewById(R.id.editTextSoB);
+        buttonCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float so1 =Float.parseFloat(editTextSo1.getText().toString());
+                float so2 =Float.parseFloat(editTextSo2.getText().toString());
+                float tong=so1+so2;
+                TextView textViewKQ = viewCau1.findViewById(R.id.textViewKetQua);
+                textViewKQ.setText(String.valueOf(tong));
+            }
+        });
+//            @Override
+//            public void onClick(View v) {
+//                String duLieuM = editText_M.getText().toString();
+//                String duLieuKM = editText_KM.getText().toString();
+//                //
+//                if (!duLieuM.isEmpty()) // user có nhập dữ liệu met, cần đổi sang km
+//                {
+//                    Toast.makeText(viewCau1.getContext(),"m -> km",Toast.LENGTH_SHORT).show();
+//                }
+//                else // đổi từ km sang met
+//                {
+//                    Toast.makeText(viewCau1.getContext(),"km -> m",Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+        return viewCau1;
+
     }
 }
--- Ex_BaiThiTongHop/app/src/main/java/java/ntu63135975/ex_baithitonghop/Cau3Fragment.java
+++ Ex_BaiThiTongHop/app/src/main/java/java/ntu63135975/ex_baithitonghop/Cau3Fragment.java
@@ -3,45 +3,34 @@ package java.ntu63135975.ex_baithitonghop;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Cau3Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Cau3Fragment extends Fragment {
+    //Khai báo biến cần thiết
+    //1. Khai báo biến toàn cục
+    LandScapeAdapter adapter;
+    ArrayList<LandScape> list;
+    //2. Khai báo các biến đại diện cho các view tương tác
+    RecyclerView recyclerViewLandScape;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Cau3Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Cau3Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Cau3Fragment newInstance(String param1, String param2) {
         Cau3Fragment fragment = new Cau3Fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +38,40 @@ public class Cau3Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        //3. Chuẩn bị dữ liệu cho List
+        list = new ArrayList<LandScape>();
+        list.add(new LandScape("eiffel","Tháp Eiffel (Pháp)"));
+        list.add(new LandScape("langhcm","Lăng Bác Hồ Chí Minh (Việt Nam)"));
+        list.add(new LandScape("nuiphusi","Núi Phú Sĩ (Nhật Bản)"));
+        list.add(new LandScape("vanlytruongthanh","Vạn Lý Trường Thành (Trung Quốc)"));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau3, container, false);
+        View viewCau3 = inflater.inflate(R.layout.fragment_cau3, container, false);
+        //4. Tìm điều khiển RycycleView
+        recyclerViewLandScape = viewCau3.findViewById(R.id.ryCau3);
+        //5. Tạo layout manager để đặt bố cục cho Recycler
+
+        //Dang 1 Doc
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(viewCau3.getContext());
+        recyclerViewLandScape.setLayoutManager(layoutLinear);
+
+        //Dang 2 Ngang
+        //RecyclerView.LayoutManager layoutLinearHorizonal = new LinearLayoutManager(viewCau3.getContext(), LinearLayoutManager.HORIZONTAL,    false);
+        //recyclerViewLandScape.setLayoutManager(layoutLinearHorizonal);
+
+        //Dang 3 Grid
+        //RecyclerView.LayoutManager layoutGrid = new GridLayoutManager(viewCau3.getContext(), 2);
+        //recyclerViewLandScape.setLayoutManager(layoutGrid);
+
+        //6. Tạo adapter gắn vào nguồn dữ liệu
+        adapter = new LandScapeAdapter(viewCau3.getContext(), list);
+        //7. Gắn adapter vào Recycler
+        recyclerViewLandScape.setAdapter(adapter);
+
+        return viewCau3;
     }
 }
